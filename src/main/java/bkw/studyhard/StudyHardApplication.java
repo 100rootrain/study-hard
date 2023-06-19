e82dfd92ca852d2c0b2367a896fd9067e191d094
qor0923 <qorehdfkr@naver.com>
2023-06-19 18:02:15 +0900

    @ComponentScan(컨트롤러경로 bkw.etc)
    html 화면이 안떴던 이유.

--- src/main/java/bkw/studyhard/StudyHardApplication.java
+++ src/main/java/bkw/studyhard/StudyHardApplication.java
@@ -2,7 +2,9 @@ package bkw.studyhard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("bkw.etc")
 @SpringBootApplication
 public class StudyHardApplication {
 
