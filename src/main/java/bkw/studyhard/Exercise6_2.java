256c509f320db457e5f33489df2aed1cc6c65b29
100rootrain <qorehdfkr@naver.com>
2023-06-21 23:23:57 +0900

    멤버변수 연습문제

---
+++ src/main/java/bkw/studyhard/Exercise6_2.java
@@ -0,0 +1,49 @@
+package bkw.studyhard;
+
+class Exercise6_2 {
+    public static void main(String args[]) {
+        Student s = new Student("홍길동",1,1,100,60,76);
+        String str = s.info();
+        System.out.println(str);
+        System.out.println((int)(236 / 3f * 10 + 0.5f));
+        System.out.println((int)(236 / 3f * 10 + 0.5f)/1f);
+        System.out.println((int)(236 / 3f * 10 + 0.5f)/.1f);
+        System.out.println((int)(236 / 3f * 10 + 0.5f)/10f);
+        System.out.println(((int)(236 / 3f * 10 + 0.5f) / 100f));
+    }
+}
+class Student {
+    String name;
+    int ban;
+    int no;
+    int kor;
+    int eng;
+    int math;
+    Student(String name, int ban, int no, int kor, int eng, int math) {
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+    public String info() {
+        return name
+                +","+ban
+                +","+no
+                +","+kor
+                +","+eng
+                +","+math
+                +","+(kor+eng+math)
+                +","+((int)((kor+eng+math) / 3f * 10 + 0.5f) / 10f)
+                ;
+    }
+
+    int getTotal(){
+        return kor+eng+math;
+    }
+
+    float getAverage(){
+        return (int)(getTotal()/3f*10+0.5f/10f);
+    }
+}
---
+++ src/main/java/bkw/studyhard/Exercise6_3.java
@@ -0,0 +1,11 @@
+package bkw.studyhard;
+
+public class Exercise6_3{
+    public static void main(String[] args){
+        Student s = new Student("백근우",1,1,100,60,76);
+
+        System.out.println("이름 : "+s.name);
+        System.out.println("총점 : "+s.getTotal());
+
+    }
+}
