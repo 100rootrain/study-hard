d82ea27db3dd22403e75b085bcaa92058aac7d5f
whitesungmin <tjdalsdlek95@naver.com>
2023-07-26 00:12:58 +0900

    연습문제7-1,7-2

---
+++ src/main/java/bkw/studyhard/exercise/Exercise7_1.java
@@ -0,0 +1,76 @@
+package bkw.studyhard.exercise;
+
+/**
+ * 섯다카드 20장을 포함하는 섯다카드 한 벌(SutdaDeck클래스)을 정의한 것이다. 섯다카드 20장을 담는 SutdaCard배열을 초기화하시오.
+ * 단, 섯다카드는 1부터 10까지의 숫자가 적힌 카드가 한쌍씩 있고, 숫자가 1,3,8인 경우에는 둘 중의 한 장은 광(Kwang)이어야 한다. 즉, SutdaCard의 인스턴스 변수 isKwang의 값이 true이어야 한다.
+ */
+class SutdaDeck {
+    final int CARD_NUM = 20;
+    SutdaCard[] cards = new SutdaCard[CARD_NUM];
+
+    SutdaDeck() {
+        /** 7-1
+         * 배열 SutdaCard를 적절히 초기화 하시오.
+         */
+
+    }
+
+
+    /** 7-2
+     * (1)위에 정의된 세 개의 메서드를 작성하시오.
+     */
+
+
+
+
+
+
+
+}
+
+
+
+class SutdaCard {
+    int num;
+    boolean isKwang;
+
+    SutdaCard() {
+        this(1, true);
+    }
+
+    SutdaCard(int num, boolean isKwang) {
+        this.num = num;
+        this.isKwang = isKwang;
+    }
+
+    //info()대신 Object클래스의 toString()을 오버라이딩했다.
+    public String toString() {
+        return num + (isKwang ? "K" : "");
+    }
+}
+
+public class Exercise7_1 {
+    public static void main(String[] args) {
+        SutdaDeck deck = new SutdaDeck();
+
+        for (int i = 0; i < deck.cards.length; i++) {
+            System.out.print(deck.cards[i] + ",");
+        }
+    }
+}
+
+/**
+ * 결과 : 1k,2,3K,4,5,6,7,8K,9,10,1,2,3,4,5,6,7,8,9,10,
+ *
+ *
+ *
+ *
+ * 답
+ * for (int i = 0; i < cards.length; i++) {
+ * int num = i % 10 + 1;
+ * boolean isKwang = (i < 10) && (num == 1 || num == 3 || num == 8);
+ * cards[i] = new SutdaCard(num, isKwang);
+ *
+ * }
+ */
+
---
+++ src/main/java/bkw/studyhard/exercise/Exercise7_2.java
@@ -0,0 +1,44 @@
+package bkw.studyhard.exercise;
+
+
+public class Exercise7_2 {
+    public static void main(String[] args) {
+        SutdaDeck deck = new SutdaDeck();
+
+        System.out.println(deck.pick(0));
+        System.out.println(deck.pick());
+        deck.shuffle();
+
+        for(int i=0; i<deck.cards.length;i++){
+            System.out.println(deck.cards[i] + ",");
+        }
+        System.out.println();
+        System.out.println(deck.pick(0));
+    }
+
+}
+
+
+/**
+ *     void shuffle(){
+ *         for(int i=0; i<cards.length;i++){
+ *             int j = (int)(Math.random()*cards.length);
+ *
+ *             //cards[i]와 cards[j]의 값을 서로 바꾼다.
+ *             SutdaCard tmp = cards[i];
+ *             cards[i] = cards[j];
+ *             cards[j] = tmp;
+ *         }
+ *     }
+ *
+ *     SutdaCard pick(int index){
+ *         if(index<0||index>=CARD_NUM) // index의 유효성을 검사한다.
+ *             return null;
+ *         return cards[index];
+ *     }
+ *
+ *     SutdaCard pick(){
+ *         int index = (int)(Math.random()*cards.length);
+ *         return pick(index); //pick(int index)를 호출한다.
+ *     }
+ */
