0fb78d063f1a5bb00df5a890480ab63be987f1a1
whitesungmin <tjdalsdlek95@naver.com>
2023-07-26 00:16:54 +0900

    주석

--- src/main/java/bkw/studyhard/exercise/Exercise7_1.java
+++ src/main/java/bkw/studyhard/exercise/Exercise7_1.java
@@ -4,6 +4,25 @@ package bkw.studyhard.exercise;
  * 섯다카드 20장을 포함하는 섯다카드 한 벌(SutdaDeck클래스)을 정의한 것이다. 섯다카드 20장을 담는 SutdaCard배열을 초기화하시오.
  * 단, 섯다카드는 1부터 10까지의 숫자가 적힌 카드가 한쌍씩 있고, 숫자가 1,3,8인 경우에는 둘 중의 한 장은 광(Kwang)이어야 한다. 즉, SutdaCard의 인스턴스 변수 isKwang의 값이 true이어야 한다.
  */
+
+
+/**
+ * 1.메서드명 : shuffle
+ *   기능 : 배열 cards에 담긴 카드의 위치를 뒤섞는다.
+ *   반환타입 : 없음
+ *   매개변수 : 없음
+ *
+ * 2.메서드명 : pick
+ *   기능 : 배열 cards에서 지정된 위치의 SutdaCard를 반환한다.
+ *   반환타입 : SutdaCard
+ *   매개변수 : int index  - 위치
+ *
+ * 3.메서드명 : pick
+ *   기능 : 배열 cards에서 임의의 위치를 StudaCard를 반환한다.
+ *   반환타입 : SutdaCard
+ *   매개변수 : djqtdma
+ *
+ */
 class SutdaDeck {
     final int CARD_NUM = 20;
     SutdaCard[] cards = new SutdaCard[CARD_NUM];
