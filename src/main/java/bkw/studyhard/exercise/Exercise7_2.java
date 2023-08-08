e5c3326f214c2b4f54bc2249f424aaef6842407c
whitesungmin <tjdalsdlek95@naver.com>
2023-08-08 21:13:52 +0900

    ex7_2

--- src/main/java/bkw/studyhard/exercise/Exercise7_2.java
+++ src/main/java/bkw/studyhard/exercise/Exercise7_2.java
@@ -5,15 +5,15 @@ public class Exercise7_2 {
     public static void main(String[] args) {
         SutdaDeck deck = new SutdaDeck();
 
-        System.out.println(deck.pick(0));
-        System.out.println(deck.pick());
-        deck.shuffle();
+//        System.out.println(deck.pick(0));
+//        System.out.println(deck.pick());
+//        deck.shuffle();
 
         for(int i=0; i<deck.cards.length;i++){
             System.out.println(deck.cards[i] + ",");
         }
         System.out.println();
-        System.out.println(deck.pick(0));
+//        System.out.println(deck.pick(0));
     }
 
 }
